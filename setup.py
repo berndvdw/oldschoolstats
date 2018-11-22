d59b70190fd84908e3dbfa37c7db75d90476c631
berndvdw <berndvdw@users.noreply.github.com>
2018-11-22 16:56:15 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+	name='oldschoolstats',
+	version='1.0',
+	description='',
+	author='Bernd van der Wielen',
+	url='',
+	packages=['Hiscores'],
+	install_requires=[
+		'requests',
+		'beautifulsoup4',                       
+	]
+)
