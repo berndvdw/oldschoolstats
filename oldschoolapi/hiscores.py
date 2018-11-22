e1b0089006a198cc04d6635e3696c8871b681ea2
berndvdw <berndvdw@users.noreply.github.com>
2018-11-22 16:46:36 +0100

    Add files via upload

--- oldschoolapi/hiscores.py
+++ oldschoolapi/hiscores.py
@@ -1,5 +1,6 @@
 import requests
 import configparser
+from collections import namedtuple
 from unidecode import unidecode
 from bs4 import BeautifulSoup
 
@@ -10,19 +11,40 @@ class Hiscores:
 		self.config.read('config.ini') # adjust this to open with OS
 		self.base_url = self.config.get('api', 'base_url')
 	
-	def getHiscorePage(self, url):
+	def getHiscorePage(self, offset=0):
 		url = "{}{}/overall.ws?table=0&page={}".format(
 				self.base_url,
 				self.config.get('types', 'normal'),
 				offset
 			)
 		
+		player = namedtuple('User', ['rank','displayname','level','xp'])
+		players = []
 		raw = requests.get(url).text
 		soup = BeautifulSoup(raw, 'html.parser')
 		for row in soup.find_all('tr',{'class':'personal-hiscores__row'}):
 			cols = [unidecode(x.text.strip()) for x in row.find_all('td')]
 			cols = [x.replace(',','') for x in cols]
-			print(cols)
+			cols = [int(col) if cols.index(col) in [0,2,3] else col for col in cols]
+			players.append(player(*cols))
+		return players
 
-	def getUser(self,name,type='normal'):
-		pass
+	def getUser(self,displayname,user_type='normal'):
+		skills = self.config.get('data_format','skills').split(',')
+		metrics = self.config.get('data_format','hierachy_skills').split(',')
+
+		url = "{}{}/{}?player={}".format(
+			self.base_url,
+			self.config.get('types', user_type),
+			self.config.get('api','endpoint'),
+			displayname
+			)
+		data = requests.get(url.format(displayname)).text.split('\n')
+		
+		obj = {}
+		for i,skill in enumerate(skills):
+			obj[skill] = {}
+			skill_data = data[i].split(",")
+			for c, part in enumerate(metrics):
+				obj[skill][part] = int(skill_data[c])
+		return {displayname: obj}
